6716431cbfef92d2e92f8e5b1dac91015989874c
AleHashirama <alehashirama@gmail.com>
2024-10-24 18:09:28 +0200

    Test

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -11,7 +11,10 @@ class TripController extends Controller
     public function index()
     {
         // Carica tutti i viaggi con le giornate e le tappe correlate
-        return response()->json(Trip::with('days.stops')->get());
+        return response()->json(Trip::with('days.stops')->get())
+        ->header('Access-Control-Allow-Origin', 'https://camper4four.netlify.app')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 
     public function showTripsPage()
