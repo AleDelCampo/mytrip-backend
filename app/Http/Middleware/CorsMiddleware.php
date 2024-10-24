de0f329ab55cbc47f5b9351d092554fb34681f52
AleHashirama <alehashirama@gmail.com>
2024-10-24 12:31:18 +0200

    Test

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -9,11 +9,11 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         if ($request->getMethod() == "OPTIONS") {
-            return response()->json(['status' => 'OK']);
+            return response()->json(['' => '204']);
         }
 
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Origin', 'https://camper4four.netlify.app')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, X-Requested-With');
     }
